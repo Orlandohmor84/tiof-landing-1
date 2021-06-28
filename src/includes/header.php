99fbcccffb86b02a1499ebb6e9cd8001f9030773
Orlando Morales <admin@ibuildweb.solutions>
2021-06-28 19:06:31 -0400

    Header Update.

--- src/includes/header.php
+++ src/includes/header.php
@@ -2,10 +2,12 @@
         <div class="container-fluid">
             <div class="container">
                 <div class="row">
-                    <div class="col-xs-12 col-sm-4">
+                    <div class="col-xs-12 col-md-4">
                         <center><img id="header-logo" alt="The Institute of F.I.L.M.S." src="..\assets\img\logo-650x396-bg-white.png"></center>
                     </div>
-                    <div class="col-xs-12 col-sm-8" id="header-phone">
+                    <div class="col-xs-12 col-md-8" id="header-phone">
+                        <h3>Enroll Now for Fall 2021. <a href="#next-steps">Apply Today.</a></h3>
+                        <h4 class="gyre">A New Non-Profit Higher Education Institute of F.I.L.M.S. Offering</h4>
                         <h2>Call Us <i class="bi bi-telephone"></i> <a href="tel:+14074801299"><span class="gyre">(407)480-1299</span></a></h2>
                     </div>
                 </div><!-- /.row -->
